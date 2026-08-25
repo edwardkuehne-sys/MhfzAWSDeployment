4cb8f960fa5e68060ae2be4aded4012836e5eedb
edwardkuehne-sys <edwardkuehne@gmail.com>
2026-08-25 12:11:57 +0200

    removed script for IP detection and added bin path to config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,25 +13,6 @@ sed -i '/^[[:space:]]*image: ghcr.io\/mezeporta\/erupe:main$/c\
     build:\
       context: ../' ~/mhfz/erupe/docker/docker-compose.yml
 
-# Get the EC2 instance's public IPv4 address
-TOKEN=$(curl -sX PUT \
-  "http://169.254.169.254/latest/api/token" \
-  -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-
-PUBLIC_IP=$(curl -s \
-  -H "X-aws-ec2-metadata-token: $TOKEN" \
-  http://169.254.169.254/latest/meta-data/public-ipv4)
-
-if [ -z "$PUBLIC_IP" ]; then
-    echo "ERROR: Could not determine public IPv4 address"
-    exit 1
-fi
-
-# Insert the actual public IP into the config
-sed "s/YOUR_PUBLIC_IP/$PUBLIC_IP/g" \
-  /opt/mhfz-deployment/config/config.json \
-  > ~/mhfz/erupe/docker/config.json
-
 echo "=== Extracting MHFZ binaries ==="
 # Download the permanent game binaries from S3
 aws s3 cp s3://my-mhfz-server-data/MHFZbinaries.7z /tmp/
