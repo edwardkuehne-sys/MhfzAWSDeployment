05e5ec3cd90ecc368bc22b141c1ed00edbee098e
edwardkuehne-sys <edwardkuehne@gmail.com>
2026-08-25 20:06:31 +0200

    added the update script and changed the config

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,30 +1,17 @@
 #!/bin/bash
 set -e
 
-# Get current public IPv4
-TOKEN=$(curl -sX PUT \
-  "http://169.254.169.254/latest/api/token" \
-  -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-
-PUBLIC_IP=$(curl -s \
-  -H "X-aws-ec2-metadata-token: $TOKEN" \
-  http://169.254.169.254/latest/meta-data/public-ipv4)
-
-if [ -z "$PUBLIC_IP" ]; then
-    echo "ERROR: Could not determine public IPv4 address"
-    exit 1
-fi
-
 # Download configuration and insert current IP
-curl -L "https://raw.githubusercontent.com/edwardkuehne-sys/MhfzAWSDeployment/refs/heads/main/config/config.json" \
+curl -L "https://raw.githubusercontent.com/ek-testlab/MhfzAWSDeployment/refs/heads/main/config/config.json" \
   -o /tmp/config.json
 
-sed "s/YOUR_PUBLIC_IP/$PUBLIC_IP/g" \
-  /tmp/config.json \
-  > ~/mhfz/erupe/docker/config.json
+echo "=== Updating Config ==="
+cp /tmp/config.json ~/mhfz/erupe/docker/config.json
 
+echo "=== Restarting Containers ==="
 cd ~/mhfz/erupe/docker
-
 sudo docker-compose down
 sudo docker-compose build
 sudo docker-compose up -d
+
+echo "=== Server Successfully Restarted ==="
