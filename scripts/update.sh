4c7e10a3843e9784b5729d7fa5c59f16a9cf1336
edwardkuehne-sys <edwardkuehne@gmail.com>
2026-08-25 20:22:51 +0200

    fixed wrong docker compose in update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 set -e
 
-# Download configuration and insert current IP
-curl -L "https://raw.githubusercontent.com/ek-testlab/MhfzAWSDeployment/refs/heads/main/config/config.json" \
-  -o /tmp/config.json
+ERUPE_DIR="/home/admin/mhfz/erupe"
+CONFIG="/home/admin/mhfz/erupe/docker/config.json"
+
+echo "=== Downloading configuration ==="
+
+curl -fL "https://raw.githubusercontent.com/ek-testlab/MhfzAWSDeployment/refs/heads/main/config/config.json" \
+  -o /tmp/mhfz-config.json
 
 echo "=== Updating Config ==="
-cp /tmp/config.json ~/mhfz/erupe/docker/config.json
+
+cp /tmp/mhfz-config.json "$CONFIG"
 
 echo "=== Restarting Containers ==="
-cd ~/mhfz/erupe/docker
-sudo docker-compose down
-sudo docker-compose build
-sudo docker-compose up -d
 
-echo "=== Server Successfully Restarted ==="
+cd "$ERUPE_DIR/docker"
+
+sudo docker compose down
+sudo docker compose build
+sudo docker compose up -d
+
+rm -f /tmp/mhfz-config.json
+
+echo "=== Deployment complete ==="
