8360aefd02bc9a80167f5e1f8981e638f6058ed3
edwardkuehne-sys <edwardkuehne@gmail.com>
2026-08-28 22:10:12 +0200

    added ip updating script

---
+++ scripts/update-ip.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+cd home/admin/mhfz/erupe/docker
+
+# Stop Erupe containers
+docker compose down
+
+# Get current public IP
+TOKEN=$(curl -sS -X PUT \
+  "http://169.254.169.254/latest/api/token" \
+  -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+PUBLIC_IP=$(curl -sS \
+  -H "X-aws-ec2-metadata-token: $TOKEN" \
+  "http://169.254.169.254/latest/meta-data/public-ipv4")
+
+echo "Public IPv4: $PUBLIC_IP"
+
+# Update config
+sed -i "s/^Host:.*/Host: $PUBLIC_IP/" config.json
+
+# Start Erupe with updated config
+docker compose up -d
